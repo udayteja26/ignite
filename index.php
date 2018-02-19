a439bd71ad631c90dfe95941419404d0a06fa387
udayteja26 <vechalapuuday@gmail.com>
2018-02-19 15:16:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+<!DOCTYPE>
+<html>
+<head>
+<link rel="stylesheet" href="bootstrap.min.css">
+<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes">
+</head>
+<body background="bg.jpg">
+<br><br>
+<div class="row">
+<div class="col-sm-4"></div>
+<div class="col-sm-4">
+<img src="ig.png" style="width:430px;">
+<br>
+<div class="row">
+<div class="col-md-6"><a class="btn btn-primary btn-block" href="userdetails/index.php">User Details </a></div>
+
+<div class="col-md-6"><a class="btn btn-primary btn-block" href="ignitedetails/index.php">Ignite details </a></div>
+</div>
+</div>
+<div class="col-sm-4"></div>
+<div class="row">
+
+</div>
+</div>
+</body>
+</html>
+
+?>
